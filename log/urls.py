ae424be17514d3007cb5f00213a9a319b4246623
Saurabh Gore <saurabhgore38@gmail.com>
2021-06-03 22:10:47 +0530

    logout added

--- log/urls.py
+++ log/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.login,name="login page"),
+    path('logout',views.logout,name="SignUP page"),
     path('signup',views.signup,name="SignUP page"),
 
 ]
--- log/views.py
+++ log/views.py
@@ -24,6 +24,9 @@ def login(request):
 	else:
 		return render(request,'login.html')
 
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 def signup(request):
 
 	if request.method=="POST":
