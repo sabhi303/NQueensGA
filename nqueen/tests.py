63c75e4a58f4b48b3bf870a17abf274251a787b1
Abhijit <abhijitshendagescoe@gmail.com>
2021-06-12 15:23:15 +0530

    Test Cases Added

--- nqueen/tests.py
+++ nqueen/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+# random testcase
+class WhateverTest(TestCase):
+    def test_True_True(self):
+        self.assertEqual("True", "True")
+        self.assertEqual("False", "False")
+
+class WhateverTest2(TestCase):
+    def test_True_True(self):
+        self.assertEqual("True", "True")
+        self.assertEqual("False", "False")
+
+class WhateverTest3(TestCase):
+    def test_True_True(self):
+        self.assertEqual("True", "True")
+        self.assertEqual("False", "False")
